4ad5db0c41f9d14d4ea604ce8e84d1eb39d74dad
master <assert2914@gmail.com>
2023-08-28 21:45:47 +0900

    package success

---
+++ src/main/java/com/focustimer/focustimer/Launcher.java
@@ -0,0 +1,7 @@
+package com.focustimer.focustimer;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,43 @@
+module com.focustimer.focustimer {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires lombok;
+    requires com.google.guice;
+    requires org.reflections;
+    requires aopalliance;
+    requires org.slf4j;
+    requires java.desktop;
+
+    opens com.focustimer.focustimer to com.google.guice, javafx.fxml;
+    exports com.focustimer.focustimer;
+
+    opens com.focustimer.focustimer.utils to com.google.guice, javafx.fxml;
+    exports com.focustimer.focustimer.utils;
+
+    opens com.focustimer.focustimer.model.timer to com.google.guice, javafx.fxml;
+    exports com.focustimer.focustimer.model.timer;
+
+    opens com.focustimer.focustimer.model.overlay to com.google.guice, javafx.fxml;
+    exports com.focustimer.focustimer.model.overlay;
+
+    opens com.focustimer.focustimer.config to com.google.guice, javafx.fxml;
+    exports com.focustimer.focustimer.config;
+
+    opens com.focustimer.focustimer.config.store to com.google.guice, javafx.fxml;
+    exports com.focustimer.focustimer.config.store;
+
+    opens com.focustimer.focustimer.components.headerBar to com.google.guice, javafx.fxml;
+    exports com.focustimer.focustimer.components.headerBar;
+
+    opens com.focustimer.focustimer.components.timerControl to com.google.guice, javafx.fxml;
+    exports com.focustimer.focustimer.components.timerControl;
+
+    opens com.focustimer.focustimer.components.timerDiskView to com.google.guice, javafx.fxml;
+    exports com.focustimer.focustimer.components.timerDiskView;
+
+    opens com.focustimer.focustimer.components.timerTextView to com.google.guice, javafx.fxml;
+    exports com.focustimer.focustimer.components.timerTextView;
+
+    opens com.focustimer.focustimer.pages.main to com.google.guice, javafx.fxml;
+    exports com.focustimer.focustimer.pages.main;
+}
